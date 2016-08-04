88d6468209c2755d0ae35a42fc56f03a741e09df
SchSascha <s.schaeuble@gmail.com>
2016-08-04 14:30:32 +0200

    Added work around to circumvent call of ValidateSBMLModelGzippedBase64Encoded, which causes the server to fail due to 'ZSI:EvaluateException\n Got None for nillable(False), minOccurs(1) element (None,ModelIsValid) ...' serverside error. Instead before validation the model is decompressed and decoded and ValidateSBMLModelText is called instead. This should be resolved in a future release.

--- src/testClient/pyClientSample.py
+++ src/testClient/pyClientSample.py
@@ -2,12 +2,15 @@ import base64
 import logging
 import zlib
 import os
+import warnings
 
+import suds
 from suds.client import Client
 
 ####
 #
-# sample Client
+# exsample Client, which works purely localy
+# PRE: server needs to run: ./bin/serverd start 
 # 
 # exemplarily an SBML is loaded, tested for validity and modified based on given data
 # the resulting two sbml files are again tested for validity and written into new files
@@ -15,56 +18,68 @@ from suds.client import Client
 ####
 
 # wsdlURL DEPRECATED: works currently but needs to be updated as soon as service moved to Tromso
-# wsdlURL			= 'http://cbu.bioinfo.no/wsdl/SBMLedit.wsdl'
+#wsdlURL			= 'http://cbu.bioinfo.no/wsdl/SBMLedit.wsdl'
+# use local version, which gets shipped with the project
 wsdlURL			= 'file://' + os.path.dirname(os.path.dirname( os.path.realpath(__file__) ))  + '/sbmlmod/SBMLmod.wsdl'
 path			= './'
 newSBMLbaseFilename	= 'newSBML'
 
+# set compression or encoding to base64
+# note that compress = True & encode = False is not supported and will be handled as compress = False, encode = False
+compress = True
+encode	 = True
+
+
 def main():
 	global path
-	compress = True
-	encode	 = True
 	
-	print(wsdlURL)
-	
-	client   = set_client(False)
+	try:
+		client   = set_client(False)
 		
-	logging.basicConfig(level=logging.INFO)
-	logging.getLogger('suds.client').setLevel(logging.CRITICAL)	
-
-	# get absolute path of running code
-	path = os.path.dirname( os.path.realpath(__file__) ) + '/'
-
-	# print minimal information about client
-	print 'Client version: ', client.service.GetVersion()
+		logging.basicConfig(level=logging.INFO)
+		logging.getLogger('suds.client').setLevel(logging.CRITICAL)	
 	
-	# get files and compress and encode them
-	files   = get_files() # order: sbml, mapping, data
-	ceFiles = compressAndEncode(files, compress, encode)
+		# get absolute path of running code
+		path = os.path.dirname( os.path.realpath(__file__) ) + '/'
 	
-	# check model validity of base model
-	print 'base SBML model validity status: ' 
-	validateModel(client, ceFiles[0], compress, encode)
+		# print minimal information about client
+		print 'Client version: ', client.service.GetVersion()
+		
+		# get files and compress and encode them
+		files   = get_files() # order: sbml, mapping, data
+		ceFiles = compressAndEncode(files)
+		
+		# check model validity of base model
+		print 'base SBML model validity status: '
+		validateModel(client, ceFiles[0])
+		
+		# manipulate models by integrating dataset-specific values
+		print 'Modifying model...'
+		newModels = manipulateModels(client, ceFiles)
+		
+		# check model validity of new model
+		print 'new SBML models validity status:'
+		validateModel(client, newModels)
+		
+		# write resulting model to new file in current folder
+		print 'Writing new model file(s) in folder' + os.getcwd()
+		writeNewModelFile(client, newModels)
 	
-	# manipulate models by integrating dataset-specific values
-	print 'Modifying model...'
-	newModels = manipulateModels(client, ceFiles)
-	
-	# check model validity of new model
-	print 'new SBML models validity status:'
-	validateModel(client, newModels, compress, encode)
-	
-	# write resulting model to new file in current folder
-	print 'Writing new model(s) to ' + os.getcwd()
-	writeNewModelFile(client, newModels, compress, encode)
-	
+	except suds.WebFault as e:
+		# show some log messages in case suds throws error  
+		print('Last sent\n')
+		print client.last_sent()
+		print('\nLast received\n')
+		print client.last_received()
+		print('Error message:\n')
+		print(e.fault)
 
 
 # ================ #
 # helper functions #
 # ================ #
 
-def writeNewModelFile(client, newModels, compress, encode):
+def writeNewModelFile(client, newModels):
 	# check whether single model as string or response and list thus, list structure from webservice is given
 	response = True
 	if isinstance(newModels, basestring):
@@ -83,7 +98,12 @@ def writeNewModelFile(client, newModels, compress, encode):
 		if ( compress and encode ):
 			newSBML = zlib.decompress( base64.b64decode(newModel) )
 		elif ( not compress and encode ):
-			newSBML = base64.b64decode(newModel)	
+			newSBML = base64.b64decode(newModel)
+		elif ( not compress and not encode ):
+			newSBML = newModel
+		else:
+			io_warn()
+			newSBML = newModel
 		
 		output.write(newSBML)
 		output.close()
@@ -100,18 +120,37 @@ def manipulateModels(client, files):
 	sbml_files.append(files[0])
 	sbml_files.append(files[0])
 
-	# build two new sbml files with replacing 'E_T' parameter values 	
-	newsbml = client.service.ReplaceGlobalParametersGzippedBase64Encoded(sbml_files, 
-																		DataFile=files[2], DataColumnNumber=3,
-																		ParameterId="E_T",
-																		MappingFile=files[1], BatchMode=True)		
+	# build two new sbml files with replacing 'E_T' parameter values 
+	
+	if ( compress and encode ):
+		newsbml = client.service.ReplaceGlobalParametersGzippedBase64Encoded(sbml_files,
+															DataFile=files[2], DataColumnNumber=3,
+															ParameterId="E_T",
+															MappingFile=files[1], BatchMode=True)	
+	elif ( not compress and encode ):
+		newsbml = client.service.ReplaceGlobalParametersBase64Encoded(sbml_files,
+															DataFile=files[2], DataColumnNumber=3,
+															ParameterId="E_T",
+															MappingFile=files[1], BatchMode=True)	
+	elif ( ( not compress ) and ( not encode ) ):
+		newsbml = client.service.ReplaceGlobalParametersText(sbml_files,
+															DataFile=files[2], DataColumnNumber=3,
+															ParameterId="E_T",
+															MappingFile=files[1], BatchMode=True)	
+	elif ( compress and not encode ):
+		io_warn()
+		newsbml = client.service.ReplaceGlobalParametersText(sbml_files,
+															DataFile=files[2], DataColumnNumber=3,
+															ParameterId="E_T",
+															MappingFile=files[1], BatchMode=True)			
+			
 	return newsbml.SbmlModelFiles
 
 
 ##
 # returns true if model is valid, false otherwise
 #
-def validateModel(client, models, compress, encode):
+def validateModel(client, models):
 	# check whether single model as string or response and list thus, list structure from webservice is given
 	response = True
 	if isinstance(models, basestring):
@@ -126,14 +165,30 @@ def validateModel(client, models, compress, encode):
 			model = model.SbmlModelFile
 					
 		if ( compress and encode ):
-			response = client.service.ValidateSBMLModelGzippedBase64Encoded(SbmlModelFile = model)
+			## WARNING: calling client.service.ValidateSBMLModelGzippedBase64Encoded
+			# throws server error on some system setups
+			# if you want to check model validity first decode and decompress and check 
+			# with client.service.ValidateSBMLModelText instead
+			# This inconvenience will be resolved in a future release
+			
+			## not working, due to error thrown by from ZSI:
+			# response = client.service.ValidateSBMLModelGzippedBase64Encoded(SbmlModelFile = model)
+			
+			# -> workaround by decoding and decompressing first and calling ValidateSBMLModelText instead:
+			response = client.service.ValidateSBMLModelText( 
+														SbmlModelFile = 
+														zlib.decompress( base64.b64decode(model) ) )
 			print 'Model is base64 encoded and compressed.'
 		elif ( not compress and encode ):
 			response = client.service.ValidateSBMLModelBase64Encoded(SbmlModelFile = model)
 			print 'Model is base64 encoded.'
-		elif ( not ( compress and encode ) ):
+		elif ( ( not compress ) and ( not encode ) ):
 			response = client.service.ValidateSBMLModelText(SbmlModelFile = model)
 			print 'Model is not encoded or compressed.'
+		elif ( compress and not encode ):
+			io_warn()
+			response = client.service.ValidateSBMLModelText(SbmlModelFile = model)
+			print 'Model is not encoded or compressed.'	
 		
 		if response.ModelIsValid:
 			print 'SBML model ', i, ' is valid.'
@@ -143,15 +198,22 @@ def validateModel(client, models, compress, encode):
 
 
 ##
-# handles compression and encoding of working files
+# handles compression and encoding of example files
 #
-def compressAndEncode(files, compress, encode):
+def compressAndEncode(files):
 	ceFiles = []
 	for i in range( 0, len(files) ):
+		# in case model contains non ASCII literals, exchange with proper unicode
+		tmp_file = ''.join([repr(c)[1:-1] if ord(c) > 128 else c for c in files[i]])	
 		if ( compress and encode ):
-			ceFiles.append( base64.b64encode(zlib.compress( files[i] ) ) )
+			ceFiles.append( base64.b64encode(zlib.compress( tmp_file ) ) )				
 		elif ( not compress and encode ):
-			ceFiles.append( base64.b64encode( files[i] ) )
+			ceFiles.append( base64.b64encode( tmp_file ) )
+		elif ( ( not compress ) and ( not encode ) ):	
+			ceFiles.append( tmp_file )
+		elif ( compress and not encode ):
+			io_warn()
+			ceFiles.append( tmp_file )
 	
 	return ceFiles
 
@@ -176,7 +238,11 @@ def set_client(show):
 		print client
 	return client
 	
-	
+##
+# simple warning message, in case user chooses wrong boolean setup (compress = False, encode = True is NOT allowed)
+#
+def io_warn():
+    warnings.warn('User defined compression, but not encoding. Please check compression and encoding setup. Using no compression instead.', Warning)
 	
 if __name__ == '__main__':
     main()
